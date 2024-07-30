f6e02aafe68358e3cc39078933678eee04ba7a65
Orphoros <orp@orphoros.dev>
2024-07-30 15:46:46 +0200

    feat: add system locale and translations

---
+++ src/systemLocale.ts
@@ -0,0 +1,10 @@
+export const systemLocale = {
+	'main-menubar': {
+		'chk-system-start': 'Start app with system',
+		'chk-rpc-autostart': 'Autostart status with app launch',
+		'lbl-status-idle': 'Idle',
+		'lbl-status-playing': 'Displaying activity',
+		'btn-start': 'Start',
+		'btn-stop': 'Stop',
+	},
+};
