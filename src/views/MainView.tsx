999c119315562a3d51097d5675d69493c3d00f4e
Orphoros <orp@orphoros.dev>
2024-08-24 19:49:11 +0200

    feat(windows): add window border if not maximized

--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {TitleBar, EditCard, PreviewCard} from '@/components';
 import {MenuOptionBuilder, useTauriContext} from '@/context';
 import {showMenu} from 'tauri-plugin-context-menu';
 import {useTranslation} from 'react-i18next';
+import {appWindow} from '@tauri-apps/api/window';
 
 function MainView() {
 	const {ipcProps, isSessionRunning, setIsSessionRunning, osType} = useTauriContext();
+	const [maximized, setMaximized] = React.useState<boolean>(false);
 	const {t: rpcHandlerTranslator} = useTranslation('lib-rpc-handle');
 	const {t: ctxMenuTranslator} = useTranslation('lib-ctx-menu');
+
+	useEffect(() => {
+		(async () => {
+			const unlisten = await appWindow.onResized(async () => {
+				const isMaximized = await appWindow.isMaximized();
+				setMaximized(isMaximized);
+			});
+			return () => {
+				unlisten();
+			};
+		})();
+	}, []);
 
 	return (
 		<div onContextMenu={
@@ -18,7 +32,7 @@ function MainView() {
 					.build());
 			}
 
-		} className='flex flex-col h-screen overflow-none'>
+		} className={`flex flex-col h-screen overflow-none ${osType !== 'Darwin' && !maximized ? 'border-gray-900 border-1' : ''}`}>
 			<TitleBar/>
 			<div className='flex flex-row overflow-hidden h-full m-3'>
 				<div className='basis-2/3 flex mr-3'>
